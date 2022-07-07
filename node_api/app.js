33bf6a83e3337ee6c0971816320c834f58356f69
Raj Kamal <937198@cognizant.com>
2022-07-07 08:06:09 +0530

    updated the view files

--- node_api/app.js
+++ node_api/app.js
@@ -3,7 +3,20 @@ let app = express();
 let port = 9000;
 let productRoutes = require("./src/routes/productRoutes");
 
+
+//middleware
+// Static File Path
+app.use(express.static(__dirname+'/public'));
+// html file path
+app.set('views','./src/views');
+// view engine name
+app.set('view engine','ejs')
+
 app.use("/products", productRoutes);
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
 
 app.listen(port, function () {
   console.log("listening on port 9000");
